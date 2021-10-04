6a9e9f057936ace349e90687bd037093757d23f7
Christy C <91734372+Christykarunya@users.noreply.github.com>
2021-10-04 22:55:34 +0530

    Add files via upload

---
+++ ClassandMethodCalculator.java
@@ -0,0 +1,24 @@
+package week1.day2;
+
+public class ClassandMethodCalculator {
+
+		public int add(int num1, int num2, int num3) {
+			int sum=num1+num2+num3;
+			return sum;
+			}
+		public int sub(int num1, int num2) {
+			int subract=num1-num2;
+			return subract;
+		  }
+		public double mul(double num1, double num2) {
+		    double multi=num1*num2;
+			return multi;
+		}
+		public float div(float num1, float num2) {
+			float div=num1/num2;
+			return div;
+		}
+	}
+
+
+
